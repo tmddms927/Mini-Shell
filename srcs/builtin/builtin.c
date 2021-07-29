0cb12daa9e2102e435f90a4ea7a79e5199bf7a73
tmddms927 <tmddms927@gmail.com>
2021-07-29 21:37:10 +0900

    fix: builtin

--- srcs/builtin/builtin.c
+++ srcs/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: seung-eun <seung-eun@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 23:00:22 by seung-eun         #+#    #+#             */
-/*   Updated: 2021/07/29 13:55:04 by seung-eun        ###   ########.fr       */
+/*   Updated: 2021/07/29 21:36:42 by seung-eun        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int	do_builtin(t_list *list, t_com *com)
 {
 	if (!com->c)
 		return (-1);
-	if (!ft_strcmp("/echo\0", com->c))
+	if (!ft_strcmp("echo\0", com->c))
 		return (echo(com->argv, 0));
-	else if (!ft_strcmp("/cd\0", com->c))
+	else if (!ft_strcmp("cd\0", com->c))
 		return (cd(list, com->argv));
-	else if (!ft_strcmp("/pwd\0", com->c))
+	else if (!ft_strcmp("pwd\0", com->c))
 		return (pwd());
-	else if (!ft_strcmp("/export\0", com->c))
+	else if (!ft_strcmp("export\0", com->c))
 		return (export(list, com));
-	else if (!ft_strcmp("/unset\0", com->c))
+	else if (!ft_strcmp("unset\0", com->c))
 		return (unset(list, com));
-	else if (!ft_strcmp("/env\0", com->c))
+	else if (!ft_strcmp("env\0", com->c))
 		return (env(list));
-	else if (!ft_strcmp("/exit\0", com->c))
+	else if (!ft_strcmp("exit\0", com->c))
 		return (ft_exit(com));
 	return (-1);
 }
